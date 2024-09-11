70da31014b66f14f576c4c2b160dbf7d77815aa9
srmnikhil <srmnikhilswn@gmail.com>
2024-09-11 07:56:11 +0530

    Remove splChar is Updated

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,18 +2,22 @@ import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
 export default function Form(props) {
+    const[text, setText] = useState("");
+    const splChar = [
+        '@', '!', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+',
+        '{', '}', '[', ']', '|', '\\', ':', ';', '"', "'", '<', '>', ',', '.',
+        '?', '/', '~', '`'
+      ];
     const handleUpClick = ()=> {
-        let newText = text.toUpperCase();
-        setText(newText);
+        setText(text.toUpperCase());
         if(text.length>0){
             props.showAlert("Your text was converted to Uppercase.", "success");
         } else {
             props.showAlert("Enter some text to convert in Uppercase.", "warning");
         }
-    }
+    };
     const handleLoClick = ()=> {
-        let newText = text.toLowerCase();
-        setText(newText);
+        setText(text.toLowerCase());
         if(text.length>0){
             props.showAlert("Your text was converted to Lowercase.", "success");
         } else {
@@ -21,28 +25,19 @@ export default function Form(props) {
         }
     }
     const handleInverseClick = () => {
-            if(text === text.toLowerCase() && text !== text.toUpperCase()){
-                handleUpClick();
-            } else if (text === text.toUpperCase() && text !== text.toLowerCase()){
-                handleLoClick();
-            }
-            else if (text !== text.toUpperCase() && text !== text.toLowerCase() && text[0].charAt(0) !== text[0].charAt(0).toLowerCase()){
-            let capWord = text.split(' ');
-            let capitalizedWords = capWord.map(words =>
-                words.charAt(0).toLowerCase() + words.slice(1).toUpperCase()
-            );
-            let capitalizedSentence = capitalizedWords.join(' ');
-            let newText = capitalizedSentence;
-            setText(newText);
-            }
-            else {
-                handleCapClick();
-            }
-            if(text.length>0){
-                props.showAlert("Your text was converted to Inversecase.", "success");
-            } else {
-                props.showAlert("Enter some text to convert in Inversecase.", "warning");
-            }
+        if (text.length > 0) {
+            let invertedText = text.split('').map(char => {
+                if (char === char.toUpperCase()) {
+                    return char.toLowerCase();
+                } else {
+                    return char.toUpperCase();
+                }
+            }).join(''); 
+            setText(invertedText);
+            props.showAlert("Your text was converted to Inversecase.", "success");
+        } else {
+            props.showAlert("Enter some text to convert in Inversecase.", "warning");
+        }
     };
     const handleCapClick = () => {
         if (text.length > 0) {
@@ -51,41 +46,49 @@ export default function Form(props) {
                 words.charAt(0).toUpperCase() + words.slice(1).toLowerCase()
             );
             let capitalizedSentence = capitalizedWords.join(' ');
-            let newText = capitalizedSentence
-            setText(newText);
+            setText(capitalizedSentence);
             props.showAlert("Your text was converted to Capitalized Words.", "success");
             } else {
-                props.showAlert("Enter some text to convert in Capitalized Words.", "warning");
+            props.showAlert("Enter some text to convert in Capitalized Words.", "warning");
             }
         }
     const handleExtraSpaces = ()=>{
         let newText = text.split(/\s+/).join(" ").trim();
         setText(newText);
-        if (text.length > 0 && newText !== text.trim()) {
-            // If text has content and newText does not match the trimmed text, there were extra spaces
+        if (text.length > 0 && newText !== text) {
             props.showAlert("Removed extra spaces from your text.", "success");
         } else if (text.length === 0) {
-            // If text is empty, prompt the user to enter some text
             props.showAlert("Enter some text to remove extra spaces.", "danger");
         } else {
-            // For all other cases, handle appropriately
             props.showAlert("Your text don't have any extra spaces.", "warning");
         }
     }
+    const handleRemoveSplChar = () =>{
+        let removeSplChar = text.split(" ");
+        let removedSplCharText = removeSplChar.map(words =>{
+            return words.split("").filter(char => !splChar.includes(char)).join("");
+        });
+        let newText = removedSplCharText.join(" ").replace(/\s+/g, " ").trim();
+        setText(newText);
+        if (newText.length === 0){
+            props.showAlert("Enter some text to remove Special Characters.", "danger");
+        } else if (newText !== text){
+            props.showAlert("Special Characters removed from your text.", "success");
+        } else {
+            props.showAlert("Your text does not have any Special Characters.", "warning");
+        }
+    }
     const handleCopy = () =>{
-        let copyText = document.getElementById("myBox");
-        copyText.select();
-        navigator.clipboard.writeText(copyText.value);
         if(text.length>0){
+            navigator.clipboard.writeText(text);
             props.showAlert("Copied to Clipboard.", "success");
         } else {
             props.showAlert("Enter some text to Copy to Clipboard.", "warning");
         }
     }
     const handleReset = ()=>{
-        let newText = "";
-        setText(newText);
         if(text.length>0){
+            setText("");
             props.showAlert("TextArea is cleared Successfully.", "success");
         } else {
             props.showAlert("TextArea is already cleared.", "warning");
@@ -94,9 +97,9 @@ export default function Form(props) {
     const handleOnChange = (event)=> {
         setText(event.target.value)
     }
-    const[text, setText] = useState("");
-    let wordCount = text.split(" ").filter(Boolean).length;
-    let readTime = 0.008 * wordCount;
+
+    const wordCount = text.split(" ").filter(Boolean).length;
+    const readTime = 0.008 * wordCount;
   return (
     <>
     <div className="container" >
@@ -109,6 +112,7 @@ export default function Form(props) {
         <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Upper Case</button>
         <button className="btn btn-primary mx-1 my-1" onClick={handleInverseClick}>Inverse Case</button>
         <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleRemoveSplChar}>Remove Special Characters</button>
         <button className="btn btn-success mx-1 my-1" onClick={handleCopy}>Copy to Clipboard</button>
         <button className="btn btn-danger mx-1 my-1" onClick={handleReset}>Reset</button>
     </div>
